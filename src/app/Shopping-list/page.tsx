0cb198e371f3ff0b5e5a7ba9cad63f6412dfbcbd
Fernando <fernandoventurini9@otmail.com>
2025-03-27 00:17:11 +0000

    setting edit button

--- src/app/Shopping-list/page.tsx
+++ src/app/Shopping-list/page.tsx
@@ -12,62 +12,80 @@ function ShoppingList() {
 
     const [items, setItems] = useState<Item[]>([]);
     const [currentItem, setCurrentItem] = useState<Item>({
-        id: 0, 
-        name: '', 
+        id: 0,
+        name: '',
         quantity: 1
     });
     const [isEditing, setIsEditing] = useState<boolean>(false);
 
-    function addItem() {
-        let id = 1;
-        if(items.length > 0) {
-            const lastItem = items[items.length - 1];
-            id = lastItem.id + 1;
-        }
+    function updateStates(item:Item ) {
+        setIsEditing(true);
+        setCurrentItem(item);
+    };
 
-        items.push({...currentItem, id});
-        setCurrentItem({id: 0, name: '', quantity: 1});
-    }
+    function process() {
+        if (!isEditing) {
+
+            let id = 1;
+
+            if (items.length > 0) {
+                const lastItem = items[items.length - 1];
+                id = lastItem.id + 1;
+            };
+
+            items.push({ ...currentItem, id });
+            setCurrentItem({ id: 0, name: "", quantity: 1 });
+        } else {
+            const updatedItems = [...items];
+            const index = items.findIndex((i => i.id === currentItem.id));
+            updatedItems[index] = { ...currentItem };
+            setItems([...updatedItems]);
+            setCurrentItem({ id: 0, name: "", quantity: 1 });
+            setIsEditing(false);
+        }
+    };
 
     return (
         <>
             <h1>LISTA DE COMPRAS</h1>
 
-            <input 
+            <input
                 value={currentItem.name}
-                type="text" 
-                placeholder="Digite o nome do produto" 
-                onChange={(e) => setCurrentItem({...currentItem, name: e.target.value})}
+                type="text"
+                placeholder="Digite o nome do produto"
+                onChange={(e) =>
+                    setCurrentItem({ ...currentItem, name: e.target.value })
+                }
             />
-            <input 
-                type="number" 
-                min={1} 
+            <input
+                type="number"
+                min={1}
                 value={currentItem.quantity}
-                onChange={(e) => setCurrentItem({...currentItem, quantity: Number(e.target.value)})}
+                onChange={(e) =>
+                    setCurrentItem({ ...currentItem, quantity: Number(e.target.value) })
+                }
             />
 
-            <button onClick={() => addItem()} style={{marginLeft: 10}}>
-                {isEditing &&
-                    'SALVAR EDICAO'    
-                }
+            <button onClick={() => process()} style={{ marginLeft: 10 }}>
+                {isEditing && "SALVAR EDICAO"}
 
-                {!isEditing &&
-                    'ADICIONAR'
-                }
+                {!isEditing && "ADICIONAR"}
             </button>
 
-            <br /><br />
+            <br />
+            <br />
 
             <ul>
-                {items.map((item) => (
+                {items.map((item, index) => (
                     <li key={item.id}>
-                        {item.name} - {item.quantity} 
-                        <button style={{marginLeft: 10}}>
+                        {item.name} - {item.quantity}
+                        <button
+                            onClick={() => updateStates(item)}
+                            style={{ marginLeft: 10 }}
+                        >
                             EDITAR
                         </button>
-                        <button style={{marginLeft: 10}}>
-                            EXCLUIR
-                        </button>
+                        <button style={{ marginLeft: 10 }}>EXCLUIR</button>
                     </li>
                 ))}
             </ul>
